d3196e806dda553f564eda5fcbf63ad9c17f9294
Tracy Yang <jewel7meme@gmail.com>
2017-03-24 19:54:16 -0500

    Completed add, delete, settings, email, sms and the floating action button.

--- app/src/main/java/css/cis3334/unit9participation_menus/MainActivity.java
+++ app/src/main/java/css/cis3334/unit9participation_menus/MainActivity.java
@@ -1,8 +1,12 @@
 package css.cis3334.unit9participation_menus;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,8 +32,10 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view,sendEmail() , Snackbar.LENGTH_LONG)
+                        //.setAction("Action", sendEmail())
+                        //.show();
+                sendEmail();
             }
         });
 
@@ -64,14 +71,25 @@ public class MainActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        switch (item.getItemId()){
+            case R.id.action_add:
+                addStudyMate();
+                return true;
+            case R.id.action_delete:
+                deleteStudyMate();
+                return true;
+            case R.id.action_email:
+                sendEmail();
+                return true;
+            case R.id.action_settings:
+            showSettings();
             return true;
+            case R.id.action_sms:
+                sendSMS();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -97,5 +115,57 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void addStudyMate(){
+        Context context = getApplicationContext();
+        CharSequence text = "Adding study mates is not available yet.";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void deleteStudyMate(){
+        Context context = getApplicationContext();
+        CharSequence text = "Deleting a study mate is not available yet.";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    public void sendEmail() {
+        Log.i("Send email", "");
+        String[] TO = {""};
+        String subject = "";
+        String bodyText = "";
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, TO);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, bodyText);
+        try {
+            startActivity(Intent.createChooser(intent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }//end of catch
+        }//end of class
+
+    public void showSettings(){
+        //calling the second activity
+        Intent intent = new Intent(this, SettingsActivity.class);
+        //start
+        startActivity(intent);
+        //startActivityForResult(intent,RESULT);
+    }
+    public void sendSMS(){
+        String phoneNumbers = "";
+        Intent sendIntent = new Intent(Intent.ACTION_SENDTO);
+        sendIntent.setData(Uri.parse("sms:" + phoneNumbers));
+        sendIntent.putExtra("sms_body", "");
+        startActivity(sendIntent);
     }
 }
